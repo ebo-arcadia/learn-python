1b74b7bccc60a88b259bddb104cf656033763b1c
Ebo Lee <yiboli.coding@gmail.com>
2021-06-01 11:03:32 -0700

    using while loop to write a car game program

--- car_game.py
+++ car_game.py
@@ -1,3 +1,32 @@
+# refactor
+command = ""
+started = False
+while True:
+    command = input(">>").lower()
+    if command == "start":
+        if started:
+            print("Car is already started! ")
+        else:
+            started = True
+            print("Car started...")
+    elif command == "stop":
+        if not started:
+            print("Car is already stopped")
+        else:
+            started = False
+            print("Car stopped")
+    elif command == "help":
+        print("""
+        start ->
+        stop ->
+        quit ->
+        """)
+    elif command == "quit":
+        break
+    else: 
+        print("please provide a valid input")
+        
+# brute force solution
 user_input = input("type help for options: ")
 
 while user_input.lower() == "help":
@@ -17,3 +46,5 @@ while user_input.lower() == "help":
 else:
     print("please enter a valid input")
         
+
+    
