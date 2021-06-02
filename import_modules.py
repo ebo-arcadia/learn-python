bd28c20cc487b61ecf193428327295ae28125565
Ebo Lee <yiboli.coding@gmail.com>
2021-06-01 18:27:22 -0700

    using modules to import methods

---
+++ import_modules.py
@@ -0,0 +1,6 @@
+import modules
+from modules import method_1
+
+print(method_1(100))
+
+print(modules.method_2(200))
---
+++ modules.py
@@ -0,0 +1,5 @@
+def method_1(weight):
+    return weight * 0.45
+
+def method_2(weight):
+    return weight / 0.45
