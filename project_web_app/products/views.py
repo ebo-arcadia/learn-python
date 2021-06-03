fbe87d848bef0308b09dc31d10ef5cca2c3aded6
Ebo Lee <yiboli.coding@gmail.com>
2021-06-03 10:34:16 -0700

    creating index and new url and map it & send it to the browser

--- project_web_app/products/views.py
+++ project_web_app/products/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Welcome to python app!')
+
+def new(request):
+    return HttpResponse('New Products')
